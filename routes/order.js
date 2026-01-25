d6fdc69e7e30f714f21b0267e66649bf1608f0e4
sary <ssbboorr11@gmail.com>
2026-01-25 22:01:54 +0200

    התאמת נתיבי ה-API וע

--- routes/order.js
+++ routes/order.js
@@ -1,24 +1,22 @@
 import express from 'express';
-import * as orderControllers from "../controllers/order.js";
+// ייבוא מפורש של כל הפונקציות מהקונטרולר
+import { 
+    getOrders, 
+    getOrderById, 
+    getOrdersByUser, 
+    addOrder, 
+    updatePaymentStatus, 
+    cancelOrder 
+} from "../controllers/order.js";
 
 const router = express.Router();
 
-// שליפת כל ההזמנות - GET api/orders
-router.get('/', orderControllers.getOrders);
-
-// שליפת הזמנה ספציפית - GET api/orders/:id
-router.get("/:id", orderControllers.getOrderById);
-
-// שליפת הזמנות לפי קוד משתמש - GET api/orders/user/:userId
-router.get("/user/:userId", orderControllers.getOrdersByUser);
-
-// הוספת הזמנה חדשה - POST api/orders
-router.post('/', orderControllers.addOrder);
-
-// עדכון סטטוס תשלום/הזמנה - PUT api/orders/:id
-router.put('/:id', orderControllers.updatePaymentStatus); // התאמה לשם הפונקציה בתיעוד
-
-// ביטול הזמנה (אם טרם שולמה) - DELETE api/orders/:id
-router.delete('/:id', orderControllers.cancelOrder);
+// שימוש ישיר בשמות הפונקציות
+router.get('/', getOrders);
+router.get("/:id", getOrderById);
+router.get("/user/:userId", getOrdersByUser);
+router.post('/', addOrder);
+router.put('/:id', updatePaymentStatus);
+router.delete('/:id', cancelOrder);
 
 export default router;
